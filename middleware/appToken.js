8bd229848222437daa515f10f96f61863ff47a30
Andrey Takhtamirov <andrey1229@yahoo.com>
2024-03-18 22:45:04 -0400

    Add check for alternative app token.

--- middleware/appToken.js
+++ middleware/appToken.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== 'production') {
 // Middleware for checking for the app token. Should be checked in all endpoints.
 const verifyAppToken = (req, res, next) => {
     const token = req.get('TRAILBLAZE-APP-TOKEN')
-    if (!token || token !== process.env.TRAILBLAZE_APP_TOKEN) {
+    if (!token || (token !== process.env.TRAILBLAZE_APP_TOKEN && token !== process.env.TRAILBLAZE_APP_TOKEN_NEW)) {
         return res.status(401).json({ message: 'Unauthorized' })
     }
     next()
